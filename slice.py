3feef922719a2f0f81b96e8d1e1d19c66e2cc9e7
victorin10 <victor.santos@in10.com.br>
2024-02-11 12:58:58 -0300

    retirando comentário para teste

--- slice.py
+++ slice.py
@@ -2,4 +2,4 @@ fruta = 'Abacate'
 # a parte [] define o index de onde será puxado o 'valor' da variável
 print (fruta [0:6])
 # isso também funciona para valores negativos, visto que os valores negativos são inversos, ou seja a posição 6 é a posição -1
-#ajsdoaijsdoiajsdoiajsdoi
+
