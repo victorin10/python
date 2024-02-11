f5ea68d3b70876db24addbfa13f5b71513a82d07
victorin10 <victor.santos@in10.com.br>
2024-02-11 19:02:00 -0300

    new file

---
+++ formatedstrings.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome: ')
+sobrenome = input ('digite seu sobrenome: ')
+profissão = input ('digite sua profissão: ')
+idade = input ('digite sua idade: ')
+texto = f'O {nome} {sobrenome} é um execelente {profissão} e tem {idade} anos'
+print(texto)
